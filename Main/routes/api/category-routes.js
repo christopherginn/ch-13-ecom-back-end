0160839c03093095ed77d2a8ba83661f0439e682
christopherginn <hey.im.chris@live.com>
2022-05-30 21:12:37 -0400

    Added DELETE route to category-route to delete category

--- Main/routes/api/category-routes.js
+++ Main/routes/api/category-routes.js
@@ -62,10 +62,28 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    { category_name: req.body.category_name },
+    { where: { id: req.params.id} }
+ )
+ .then(() => res.json("Category updated"))
+ .catch((err) => {
+   // console.log(err);
+   res.status(400).json(err);
+   });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => {
+      res.json(`Category deleted.`);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
